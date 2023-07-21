a244a83d5533988c94e569c2a7fda1522b9f7bb3
Moritz Lang <moritz.lang@technikum-wien.at>
2023-07-21 23:40:57 +0200

    Initial import

---
+++ resources/profikum_arduino/ProfikumDevice.cpp
@@ -0,0 +1,140 @@
+// Copyright 2023 Moritz Lang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include "ProfikumSupersonic.h"
+#include "ProfikumDevice.h"
+#include "ProfikumMotors.h"
+#include "ProfikumImu.h"
+#include "ProfikumEncoders.h"
+
+#include <Arduino.h>
+
+#define MAX_SPEED 400
+
+namespace profikum::arduino
+{
+static ProfikumMotors motors{};
+// accelerometer, magnetometer, and gyro
+static ProfikumImu imu{};
+static ProfikumSupersonic superSonic{};
+static ProfikumEncoders encoders{};
+}
+
+/**
+ * Interrupt handlers
+ */
+ISR(PCINT0_vect)
+{
+  profikum::arduino::superSonic.OnInterrupt();
+  profikum::arduino::encoders.OnLeftInterrupt();
+}
+// interrupt pin 7
+static void interrupt4()
+{
+  profikum::arduino::encoders.OnRightInterrupt();
+}
+
+
+namespace profikum::arduino
+{
+ProfikumDevice::ProfikumDevice()
+{
+}
+void ProfikumDevice::Init(void (*outputProcessor_)(com::ProfikumOutput, int16_t))
+{
+  outputProcessor = outputProcessor_;
+  
+  // Init IMU sensors.
+  if (!imu.Init())
+  {
+    // If initialization failed, send error message in endless loop.
+    while(1)
+    {
+      outputProcessor(profikum::com::ProfikumOutput::error, -1);
+      delay(1000);
+    }
+  }
+  imu.EnableDefault();
+
+  superSonic.Init();
+  motors.Init();
+  encoders.Init();
+
+  attachInterrupt(4, interrupt4, CHANGE);
+}
+void ProfikumDevice::ProcessInput(com::ProfikumInput command, int16_t value)
+{
+  switch(command)
+  {
+    case com::ProfikumInput::leftMotorSetSpeed:
+      leftSpeed = value;
+      if(leftSpeed > MAX_SPEED)
+        leftSpeed = MAX_SPEED;
+      else if(leftSpeed < -MAX_SPEED)
+        leftSpeed = -MAX_SPEED;
+      break;
+    case com::ProfikumInput::rightMotorSetSpeed:
+      rightSpeed = value;
+      if(rightSpeed > MAX_SPEED)
+        rightSpeed = MAX_SPEED;
+      else if(rightSpeed < -MAX_SPEED)
+        rightSpeed = -MAX_SPEED;
+      break;
+    case com::ProfikumInput::error:
+      leftSpeed = 0;
+      rightSpeed = 0;
+      break;
+  }
+}
+
+
+
+void ProfikumDevice::Run()
+{
+  motors.SetLeftSpeed(leftSpeed);
+  motors.SetRightSpeed(rightSpeed);
+
+  superSonic.Run();
+  encoders.Run();
+
+  if(outputProcessor != nullptr)
+  {
+    imu.Read();
+    // Accelleration
+    outputProcessor(com::ProfikumOutput::accelerationX, imu.a.x);
+    outputProcessor(com::ProfikumOutput::accelerationY, imu.a.y);
+    outputProcessor(com::ProfikumOutput::accelerationZ, imu.a.z);
+    // Gyro 
+    outputProcessor(com::ProfikumOutput::gyroX, imu.g.x);
+    outputProcessor(com::ProfikumOutput::gyroY, imu.g.y);
+    outputProcessor(com::ProfikumOutput::gyroZ, imu.g.z);
+    // magnetometer 
+    outputProcessor(com::ProfikumOutput::magnetometerX, imu.m.x);
+    outputProcessor(com::ProfikumOutput::magnetometerY, imu.m.y);
+    outputProcessor(com::ProfikumOutput::magnetometerZ, imu.m.z);
+    // Ultrasound distance
+    outputProcessor(com::ProfikumOutput::ultrasoundDistance, superSonic.GetLastDistance_mm());
+    // encoders
+    outputProcessor(com::ProfikumOutput::leftEncoderCounts, encoders.GetCountsLeft());
+    outputProcessor(com::ProfikumOutput::rightEncoderCounts, encoders.GetCountsRight());
+    outputProcessor(com::ProfikumOutput::leftEncoderCountsPerSecond, encoders.GetCountsPerSecondLeft());
+    outputProcessor(com::ProfikumOutput::rightEncoderCountsPerSecond, encoders.GetCountsPerSecondRight());
+  }
+}
+}
---
+++ src/Profikum.h
@@ -0,0 +1,69 @@
+#ifndef PROFIKUM_H
+#define PROFIKUM_H
+
+#pragma once
+
+#include "Profinet.h"
+#include "profikum_com.h"
+
+class SerialConnection;
+
+class Profikum final
+{
+public:
+    Profikum();
+    ~Profikum();
+    
+    Profikum (const Profikum&) = delete;
+    Profikum& operator= (const Profikum&) = delete;
+
+    bool InitializeProfinet(const std::string_view& mainNetworkInterface="wlan0");
+    bool StartProfinet();
+    bool ExportGDSML(const char* filename) const;
+    void RunController(const std::string_view& serialPort="/dev/ttyAMA0");
+private:
+    profinet::LoggerType logger;
+    void Log(profinet::LogLevel logLevel, const char* format, ...) noexcept;    
+    bool profinetInitialized{false};
+    profinet::Profinet profinet;
+    
+    //Outputs
+    int16_t speedLeft{0};
+    int16_t speedRight{0};
+    
+    // Inputs
+    //Acceleration
+    int16_t accelerationX{0};
+    int16_t accelerationY{0};
+    int16_t accelerationZ{0};
+    // Gyro 
+    int16_t gyroX{0};
+    int16_t gyroY{0};
+    int16_t gyroZ{0};
+    // Magnetometer 
+    int16_t magnetometerX{0};
+    int16_t magnetometerY{0};
+    int16_t magnetometerZ{0};
+    // ultrasound
+    int16_t distance{-1};
+    // encoder
+    int16_t leftEncoderCounts{0};
+    int16_t rightEncoderCounts{0};
+    int16_t leftEncoderCountsPerSecond{0};
+    int16_t rightEncoderCountsPerSecond{0};
+
+    // Parameters
+    // encoder
+    uint16_t countsPerRotation{910};
+    uint16_t wheelRadius_mm{19};
+
+    std::unique_ptr<profinet::ProfinetControl> profinetInstance;
+
+    bool SendSerial(SerialConnection& serialConnection);
+    bool ReceiveSerial(SerialConnection& serialConnection);
+    bool InterpretCommand(profikum::com::ProfikumOutput command, int16_t value);
+
+    static bool IsInterfaceOnline(std::string interface);
+};
+
+#endif
